45d9329ed9fa74f5da2b6975d30654eec38f519e
Cicak Bin Kadal <70104051+cbkadal@users.noreply.github.com>
2021-05-17 23:29:44 +0700

    new file:   W08/WEEK08-REPORT-CH02.txt

--- SandBox/W08/do_ch2.sh
+++ SandBox/W08/do_ch2.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+# REV04 Mon 17 May 23:28:08 WIB 2021
 # REV03 Mon 17 May 14:38:33 WIB 2021
 # REV02 Sun 16 May 11:40:33 WIB 2021
 # REV01 Sat 15 May 22:25:26 WIB 2021
 # START Fri 14 May 14:29:10 WIB 2021
 
-CHAPTER="04"
-WEEK="09"
+CHAPTER="02"
+WEEK="08"
 FILE="WEEK$WEEK-REPORT-CH$CHAPTER.txt"
 
 # WEEKDIR="$HOME/RESULT/W$WEEK/"
@@ -37,42 +38,27 @@ fecho(){
 mkdir -pv $WEEKDIR
 [ -d $WEEKDIR ] && cd $WEEKDIR || byebye "Error: No $WEEKDIR"
 [ -d $LFSDIR ]                 || byebye "Error: No $LFSDIR"
-[ -d $LFSDIR/sources/ ]        || byebye "Error: No $LFSDIR/sources/"
-[ -f $LFSDIR/sources/md5sums ] || byebye "Error: No $LFSDIR/sources/md5sums"
 rm -f $FILE
 fecho "===== Chapter $CHAPTER =====  Week $WEEK ====="
 fecho "pwd $(pwd)"
 fecho "date $(date +%y%m%d-%H%M)"
 fecho "hostname $(hostname)"
 fecho "username $(whoami)"
-fecho "Please Wait..."
-fecho "okSources $(okSources)"
-fecho "READY!"
-fecho "Sources  $(du -s -h $LFSDIR/sources/)"
-
 MaxLFS=0
 MaxROOT=0
 MaxMemory=0
 MaxSwap="-1"
 EXTRAS=60
-while true ; do
-    TOKEN=$(chktokenn $INPUTTOKEN)
-    VERIFY=$(verifyTokenn $INPUTTOKEN $TOKEN)
-    fecho "verifyTokenn $INPUTTOKEN $TOKEN $VERIFY"
-    LOOP=10
-    while (( LOOP-- )) ; do
-        TMP1=$(($(df|awk '/ \/mnt\/lfs$/ {print $3}')/1024))
-        (( "$MaxLFS" < "$TMP1" )) && { MaxLFS=$TMP1; fecho "MaxLFS ${MaxLFS}M" ; }
-        TMP1=$(($(df|awk '/ \/$/ {print $3}')/1024))
-        (( "$MaxROOT" < "$TMP1" )) && { MaxROOT=$TMP1; fecho "MaxROOT ${MaxROOT}M" ; }
-        TMP1=$(($(free|awk '/Mem:/ {print $3}')/1024))
-        (( "$MaxMemory" < "$TMP1" )) && { MaxMemory=$TMP1; fecho "MaxMemory ${MaxMemory}M" ; }
-        TMP1=$(($(free|awk '/Swap:/ {print $3}')/1024))
-        (( "$MaxSwap" < "$TMP1" )) && { MaxSwap=$TMP1; fecho "MaxSwap ${MaxSwap}M" ; }
-        sleep 6
-    done
-    sleep $((1+(++EXTRAS/60)))
-    (( "$EXTRAS" > "60" )) && EXTRAS=0
-done
+TOKEN=$(chktokenn $INPUTTOKEN)
+VERIFY=$(verifyTokenn $INPUTTOKEN $TOKEN)
+fecho "verifyTokenn $INPUTTOKEN $TOKEN $VERIFY"
+TMP1=$(($(df|awk '/ \/mnt\/lfs$/ {print $3}')/1024))
+(( "$MaxLFS" < "$TMP1" )) && { MaxLFS=$TMP1; fecho "MaxLFS ${MaxLFS}M" ; }
+TMP1=$(($(df|awk '/ \/$/ {print $3}')/1024))
+(( "$MaxROOT" < "$TMP1" )) && { MaxROOT=$TMP1; fecho "MaxROOT ${MaxROOT}M" ; }
+TMP1=$(($(free|awk '/Mem:/ {print $3}')/1024))
+(( "$MaxMemory" < "$TMP1" )) && { MaxMemory=$TMP1; fecho "MaxMemory ${MaxMemory}M" ; }
+TMP1=$(($(free|awk '/Swap:/ {print $3}')/1024))
+(( "$MaxSwap" < "$TMP1" )) && { MaxSwap=$TMP1; fecho "MaxSwap ${MaxSwap}M" ; }
 exit
 
--- SandBox/W08/do_ch4.sh
+++ SandBox/W08/do_ch4.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# REV04 Mon 17 May 22:41:13 WIB 2021
+# REV04 Mon 17 May 23:28:47 WIB 2021
 # REV03 Mon 17 May 14:38:33 WIB 2021
 # REV02 Sun 16 May 11:40:33 WIB 2021
 # REV01 Sat 15 May 22:25:26 WIB 2021
 # START Fri 14 May 14:29:10 WIB 2021
 
-CHAPTER="02"
+CHAPTER="04"
 WEEK="09"
 FILE="WEEK$WEEK-REPORT-CH$CHAPTER.txt"
 
@@ -38,27 +38,42 @@ fecho(){
 mkdir -pv $WEEKDIR
 [ -d $WEEKDIR ] && cd $WEEKDIR || byebye "Error: No $WEEKDIR"
 [ -d $LFSDIR ]                 || byebye "Error: No $LFSDIR"
+[ -d $LFSDIR/sources/ ]        || byebye "Error: No $LFSDIR/sources/"
+[ -f $LFSDIR/sources/md5sums ] || byebye "Error: No $LFSDIR/sources/md5sums"
 rm -f $FILE
 fecho "===== Chapter $CHAPTER =====  Week $WEEK ====="
 fecho "pwd $(pwd)"
 fecho "date $(date +%y%m%d-%H%M)"
 fecho "hostname $(hostname)"
 fecho "username $(whoami)"
+fecho "Please Wait..."
+fecho "okSources $(okSources)"
+fecho "READY!"
+fecho "Sources  $(du -s -h $LFSDIR/sources/)"
+
 MaxLFS=0
 MaxROOT=0
 MaxMemory=0
 MaxSwap="-1"
 EXTRAS=60
-TOKEN=$(chktokenn $INPUTTOKEN)
-VERIFY=$(verifyTokenn $INPUTTOKEN $TOKEN)
-fecho "verifyTokenn $INPUTTOKEN $TOKEN $VERIFY"
-TMP1=$(($(df|awk '/ \/mnt\/lfs$/ {print $3}')/1024))
-(( "$MaxLFS" < "$TMP1" )) && { MaxLFS=$TMP1; fecho "MaxLFS ${MaxLFS}M" ; }
-TMP1=$(($(df|awk '/ \/$/ {print $3}')/1024))
-(( "$MaxROOT" < "$TMP1" )) && { MaxROOT=$TMP1; fecho "MaxROOT ${MaxROOT}M" ; }
-TMP1=$(($(free|awk '/Mem:/ {print $3}')/1024))
-(( "$MaxMemory" < "$TMP1" )) && { MaxMemory=$TMP1; fecho "MaxMemory ${MaxMemory}M" ; }
-TMP1=$(($(free|awk '/Swap:/ {print $3}')/1024))
-(( "$MaxSwap" < "$TMP1" )) && { MaxSwap=$TMP1; fecho "MaxSwap ${MaxSwap}M" ; }
+while true ; do
+    TOKEN=$(chktokenn $INPUTTOKEN)
+    VERIFY=$(verifyTokenn $INPUTTOKEN $TOKEN)
+    fecho "verifyTokenn $INPUTTOKEN $TOKEN $VERIFY"
+    LOOP=10
+    while (( LOOP-- )) ; do
+        TMP1=$(($(df|awk '/ \/mnt\/lfs$/ {print $3}')/1024))
+        (( "$MaxLFS" < "$TMP1" )) && { MaxLFS=$TMP1; fecho "MaxLFS ${MaxLFS}M" ; }
+        TMP1=$(($(df|awk '/ \/$/ {print $3}')/1024))
+        (( "$MaxROOT" < "$TMP1" )) && { MaxROOT=$TMP1; fecho "MaxROOT ${MaxROOT}M" ; }
+        TMP1=$(($(free|awk '/Mem:/ {print $3}')/1024))
+        (( "$MaxMemory" < "$TMP1" )) && { MaxMemory=$TMP1; fecho "MaxMemory ${MaxMemory}M" ; }
+        TMP1=$(($(free|awk '/Swap:/ {print $3}')/1024))
+        (( "$MaxSwap" < "$TMP1" )) && { MaxSwap=$TMP1; fecho "MaxSwap ${MaxSwap}M" ; }
+        sleep 6
+    done
+    sleep $((1+(++EXTRAS/60)))
+    (( "$EXTRAS" > "60" )) && EXTRAS=0
+done
 exit
 
